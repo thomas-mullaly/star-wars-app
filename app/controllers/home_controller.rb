a3d964e6c0ad179247082cc0680f65c0904e55f5
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-04-21 13:53:48 -0400

    Generate list of movies on home page. Added some basic tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+      @films = Tatooine::Film.list.sort_by { |film| film.episode_id }
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,32 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get all 7 films" do
+      get :index
+      assert_not_nil assigns(:films)
+      assert_equal 7, assigns(:films).length
+  end
+
+  test "should order all by films by episode id" do
+      get :index
+
+      for i in 0..6
+          film = assigns(:films)[i]
+
+          assert_equal i+1, film.episode_id
+      end
+  end
+
+  test "should generate the correct film cards" do
+      get :index
+
+      assert_select ".film" do |cards|
+          assert_equal 7, cards.length
+
+          cards.each do |card|
+              assert_select card, "a", 1
+              assert_select card, "a img", 1
+          end
+      end
+  end
 end
