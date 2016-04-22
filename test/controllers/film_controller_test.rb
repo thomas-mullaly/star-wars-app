d80d71b70a2e8a1549e6e955044ed69365070bb1
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-04-22 12:12:50 -0400

    Added VCR for controller tests

--- test/controllers/film_controller_test.rb
+++ test/controllers/film_controller_test.rb
@@ -1,22 +1,29 @@
+require 'vcr_setup'
 require 'test_helper'
 
 class FilmControllerTest < ActionController::TestCase
     test "should get index" do
-        get :index, :id => 1
+        VCR.use_cassette("film controller tests") do
+            get :index, :id => 1
 
-        assert_response :success
-        assert_not_nil assigns(:film)
+            assert_response :success
+            assert_not_nil assigns(:film)
+        end
     end
 
     test "should set h1 to film title" do
-        get :index, :id => 1
+        VCR.use_cassette("film controller tests") do
+            get :index, :id => 1
 
-        assert_select "h1", "A New Hope"
+            assert_select "h1", "A New Hope"
+        end
     end
 
     test "should set opening crawl text" do
-        get :index, :id => 1
+        VCR.use_cassette("film controller tests") do
+            get :index, :id => 1
 
-        assert_select ".crawl p", 3
+            assert_select ".crawl p", 3
+        end
     end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,37 +1,46 @@
+require 'vcr_setup'
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+    test "should get index" do
+        VCR.use_cassette("home controller tests") do
+            get :index
+            assert_response :success
+        end
+    end
 
-  test "should get all 7 films" do
-      get :index
-      assert_not_nil assigns(:films)
-      assert_equal 7, assigns(:films).length
-  end
+    test "should get all 7 films" do
+        VCR.use_cassette("home controller test") do
+            get :index
+            assert_not_nil assigns(:films)
+            assert_equal 7, assigns(:films).length
+        end
+    end
 
-  test "should order all by films by episode id" do
-      get :index
+    test "should order all by films by episode id" do
+        VCR.use_cassette("home controller test") do
+            get :index
 
-      for i in 0..6
-          film = assigns(:films)[i]
+            for i in 0..6
+                film = assigns(:films)[i]
 
-          assert_equal i+1, film.episode_id
-      end
-  end
+                assert_equal i+1, film.episode_id
+            end
+        end
+    end
 
-  test "should generate the correct film cards" do
-      get :index
+    test "should generate the correct film cards" do
+        VCR.use_cassette("home controller test") do
+            get :index
 
-      assert_select ".film" do |cards|
-          assert_equal 7, cards.length
+            assert_select ".film" do |cards|
+                assert_equal 7, cards.length
 
-          cards.each do |card|
-              assert_select card, "a", 1
-              assert_select card, "a img", 1
-          end
-      end
-  end
+                cards.each do |card|
+                    assert_select card, "a", 1
+                    assert_select card, "a img", 1
+                end
+            end
+        end
+    end
 end
